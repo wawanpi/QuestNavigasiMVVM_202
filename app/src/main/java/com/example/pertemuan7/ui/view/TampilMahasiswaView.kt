c2072593adda3d7da1026da538752d3603b000af
wawanpi <muhaguskurniawan@gmail.com>
2024-11-17 18:54:06 +0700

    Menambahkan text Data Mahasiswa di TampilMahasiswaView.kt

--- app/src/main/java/com/example/pertemuan7/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/pertemuan7/ui/view/TampilMahasiswaView.kt
@@ -3,13 +3,17 @@ package com.example.pertemuan7.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pertemuan7.Model.Mahasiswa
 
 @Composable
@@ -17,6 +21,16 @@ fun TampilMahasiswaView(
     mhs: Mahasiswa
 ){
     Column (  modifier = Modifier.fillMaxSize()){
+        Text(text = "Data Mahasiswa" ,
+            fontWeight = FontWeight.Bold ,
+            fontSize = 28.sp,
+            modifier = Modifier
+                .fillMaxWidth() // Mengisi seluruh lebar layar)
+                .padding(vertical = 16.dp),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.padding (20.dp))
+
         TampilData(
             judul = "Nama",
             isinya = mhs.nama
